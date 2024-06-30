b261e1462dfa17882e933dcf44b3958f80ac8970
Om Rabadia <orrabadia@gmail.com>
2024-06-29 22:44:36 -0700

    Added DOMContentLoaded Event Listener

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 
-document.getElementById("date").innerHTML += " " + getCurrentDayOfWeek() + '!'
+document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById("date").innerHTML += " " + getCurrentDayOfWeek() + '!';
 
 // date script
 function getCurrentDayOfWeek() {
    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
    const d = new Date()
    return daysOfWeek[d.getDay()]
-}
+}})
