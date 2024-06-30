d0256f20d64f5e0d56cd984260a5d1e64ce8ae96
Om Rabadia <orrabadia@gmail.com>
2024-06-29 22:30:06 -0700

    Fix date array and start CSS code

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 document.getElementById("date").innerHTML += " " + getCurrentDayOfWeek() + '!'
 
-//date script
+// date script
 function getCurrentDayOfWeek() {
-   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Friday", "Saturday"];
+   const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
    const d = new Date()
    return daysOfWeek[d.getDay()]
 }
